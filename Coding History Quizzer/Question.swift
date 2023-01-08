cd9d3d2dbb4856bfa3b237cf55c258365c3d90c1
Andrew Perkins <andrewperk@gmail.com>
2023-01-08 13:28:30 -0800

    Created Question model, ChoiceTextView and refactored view

---
+++ Coding History Quizzer/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Coding History Quizzer
+//
+//  Created by Andrew L Perkins on 1/8/23.
+//
+
+import Foundation
+
+struct Question {
+    var questionText: String
+    var possibleAnswers: [String]
+    var correctAnswerIndex: Int
+}
