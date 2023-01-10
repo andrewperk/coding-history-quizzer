347a9bdb821d66a4ee9783dbdf8268391884f207
Andrew Perkins <andrewperk@gmail.com>
2023-01-10 14:21:54 -0800

    Created first logic for handling the quiz

--- Coding History Quizzer/Models/Question.swift
+++ Coding History Quizzer/Models/Question.swift
@@ -7,8 +7,27 @@
 
 import Foundation
 
-struct Question {
+struct Question: Hashable { // Add Hashable Conformance
     var questionText: String
     var possibleAnswers: [String]
     var correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web",
+                 possibleAnswers: [
+                    "Steve Jobs",
+                    "Linus Torvalds",
+                    "Bill Gates",
+                    "Tim Berners-Lee"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                    "Simula",
+                    "Python",
+                    "Swift",
+                    "C"
+                 ],
+                 correctAnswerIndex: 0)
+    ]
 }
