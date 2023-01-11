e291aadcbf1dbe8ac3672c55391cfe334918fb9d
Andrew Perkins <andrewperk@gmail.com>
2023-01-10 23:20:15 -0800

    Changed minimum deployment, created WelcomeView, refactored ContentView to GameView

--- Coding History Quizzer/Coding_History_QuizzerApp.swift
+++ Coding History Quizzer/Coding_History_QuizzerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Coding_History_QuizzerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WelcomeView()
         }
     }
 }
---
+++ Coding History Quizzer/Views/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  Coding History Quizzer
+//
+//  Created by Andrew L Perkins on 1/10/23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
