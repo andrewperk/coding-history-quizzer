93118b1f2d54bdfe0302678ebb4d9f5bb0cea93e
Andrew Perkins <andrewperk@gmail.com>
2023-01-15 20:03:15 -0800

    Revert ".gitignore now works"
    This reverts commit 908e4001795107410fd26dc023f4f23e43b48c54.

---
+++ Coding History Quizzer/Coding_History_QuizzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_History_QuizzerApp.swift
+//  Coding History Quizzer
+//
+//  Created by Andrew L Perkins on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Coding_History_QuizzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
